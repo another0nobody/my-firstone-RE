76d31ca89bbdb20286020a21b38a04da8c60675d
lzdok <liu654612146@163.com>
2022-12-03 21:51:56 +0800

    组织架构静态树状图完成

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,14 +1,52 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>组织架构</h2>
+      <!-- 实现页面的基本布局 -->
+      <el-card class="tree-card">
+        <TreeTool :tree-node="company" :is-root="true" />
+        <el-tree :data="departs" :props="defaultProps" default-expand-all>
+          <TreeTool slot-scope="{ data }" :tree-node="data" :is-root="false" />
+        </el-tree>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import TreeTool from './components/tree-tools.vue'
+export default {
+  components: {
+    TreeTool
+  },
+  data() {
+    return {
+      company: { name: '某某有限公司', manager: '负责人' },
+      defaultProps: {
+        label: 'name',
+        children: 'children'
+      },
+      departs: [
+        {
+          name: '总裁办',
+          manager: '曹操',
+          children: [{ name: '董事会', manager: '曹丕' }]
+        },
+        { name: '行政部', manager: '刘备' },
+        { name: '人事部', manager: '孙权' }
+      ]
+    }
+  },
+  methods: {
+    handleNodeClick(data) {
+      console.log(data)
+    }
+  }
+}
 </script>
 
 <style>
+.tree-card {
+  padding: 30px 140px;
+  font-size: 14px;
+}
 </style>
